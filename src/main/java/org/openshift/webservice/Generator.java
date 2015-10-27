5c38c2860bb2cabaae2cfe92a98d6f16b309e329
Steven Pousty <spousty@redhat.com>
2015-10-27 13:09:50 -0700

    should work now

--- src/main/java/org/openshift/webservice/Generator.java
+++ src/main/java/org/openshift/webservice/Generator.java
@@ -1,6 +1,14 @@
 package org.openshift.webservice;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.util.JsonSerialization;
 import org.openshift.model.*;
 import org.openshift.model.Character;
 
@@ -10,6 +18,8 @@ import javax.ws.rs.client.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 /**
@@ -25,13 +35,13 @@ public class Generator {
     private SecurityContext securityContext;
 
     @GET()
-    @Produces("text/json")
+    @Produces("application/json")
     public HashMap MakeACharacter(){
         return character.getAllAttributes();
     }
 
     @GET()
-    @Produces("text/json")
+    @Produces("application/json")
     @Path("{name}")
     public HashMap MakeACharacterWithAName(@PathParam("name") String name){
 
@@ -41,10 +51,9 @@ public class Generator {
     }
 
     @GET()
-    @Produces("text/json")
+    @Produces("application/json")
     @Path("dd")
-
-    public HashMap MakeACharacterForMongo(@Context HttpServletRequest request){
+    public InputStream MakeACharacterForMongo(@Context HttpServletRequest request) throws Exception{
 
         ///get the userid from keycloak here and use it for the name
         String name = securityContext.getUserPrincipal().getName();
@@ -59,13 +68,37 @@ public class Generator {
         String token = mySecurityContext.getTokenString();
 
         //build the POST
+
+        HttpClient client = new DefaultHttpClient();
+        try {
+            HttpPost post = new HttpPost("https://j1mongo-thesteve0.rhcloud.com/ws/players");
+            post.addHeader("Authorization", "Bearer " + token);
+            post.addHeader("Content-Type", "application/json");
+                post.setEntity(new StringEntity(JsonSerialization.writeValueAsString(character.getAllAttributes())));
+                HttpResponse response = client.execute(post);
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new RuntimeException("request failed for some non-helpful reason");
+                }
+
+                HttpEntity entity = response.getEntity();
+                InputStream is = entity.getContent();
+                try {
+                    return is;
+                } finally {
+                    is.close();
+                }
+
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+        /*
         Client client = ClientBuilder.newClient();
         WebTarget resourceTarget = client.target("https://j1mongo-thesteve0.rhcloud.com/ws/players");
         Invocation.Builder invocationBuilder = resourceTarget.request("application/json").header("Authorization", "bearer " + token);
         Invocation invocation = invocationBuilder.buildPost(Entity.json(character));
         Response response = invocation.invoke();
-
-        System.out.println("response code: " + response.getStatus());
+        */
+        //System.out.println("response code: " + response.getStatus());
 
         /*
 
@@ -73,7 +106,7 @@ public class Generator {
         https://jax-rs-spec.java.net/nonav/2.0-SNAPSHOT/apidocs/javax/ws/rs/client/WebTarget.html
         https://jax-rs-spec.java.net/nonav/2.0-SNAPSHOT/apidocs/javax/ws/rs/client/Invocation.Builder.html
          */
-        return character.getAllAttributes();
+        //return character.getAllAttributes();
 
     }
 
